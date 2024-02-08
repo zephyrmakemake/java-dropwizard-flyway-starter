e2aab56537948855aebb8db99353b4ef57bdc01a
shaunganley <s.ganley@kainos.com>
2024-02-08 12:03:54 +0000

    setup flyway plugin

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# java-swagger-flyway-starter
+# Java DropWizard Flyway Starter
+
+Database Migration
+---
+
+1. Add the following lines to your ~/.zshrc file:
+```
+export FLYWAY_URL="jdbc:mysql://YOUR_DB_HOST/YOUR_DB_NAME"
+export FLYWAY_USER="YOUR_DB_USER"
+export FLYWAY_PASSWORD="YOUR_DB_PASSWORD"
+export FLYWAY_BASELINE_ON_MIGRATE=true
+```
+2. Reload your terminal session if required:
+```
+. ~/.zshrc
+```
+3. Run Flyway command through Maven:
+```
+mvn flyway:migrate
+```
+
+How to start the true application
+---
+
+1. Run `mvn clean install` to build your application
+1. Start application with `java -jar target/java-swagger-flyway-starter-org.kainos.ea.jar server config.yml`
+1. To check that your application is running enter url `http://localhost:8080`
+
+Health Check
+---
+
+To see your applications health enter url `http://localhost:8081/healthcheck`
---
+++ src/main/java/org/example/db/DatabaseConnector.java
@@ -0,0 +1,32 @@
+package org.example.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+    private static Connection conn;
+
+    public static Connection getConnection() throws SQLException {
+
+        if (conn != null && !conn.isClosed()) { return conn; }
+
+        try {
+            String username = System.getenv().get("DB_USERNAME");
+            String password = System.getenv().get("DB_PASSWORD");
+            String host = System.getenv().get("DB_HOST");
+            String name = System.getenv().get("DB_NAME");
+
+            if (username == null || password == null || host == null || name == null)
+                throw new IllegalArgumentException("Add the following properties to env vars: DB_USERNAME, DB_PASSWORD, DB_HOST and DB_NAME");
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://" + host + "/" + name, username, password);
+            return conn;
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        return null;
+    }
+}
