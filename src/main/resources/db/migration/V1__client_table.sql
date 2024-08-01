1668324bca8c35278166f034b74da3310756480b
Patryk Podlewski <patryk.podlwski@kainos.com>
2024-08-01 14:06:47 +0200

    refactoring id in client

--- src/main/resources/db/migration/V1__client_table.sql
+++ src/main/resources/db/migration/V1__client_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE client (
-    id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    clientID int PRIMARY KEY AUTO_INCREMENT NOT NULL,
     name varchar(70) NOT NULL,
     address varchar(200) NOT NULL,
     phone varchar(64) NOT NULL
